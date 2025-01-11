a63ba7a1b1a5bbed2288a80a0d3b9282ef0c221e
김민희 <49574992+minheeKim3235@users.noreply.github.com>
2025-01-12 00:49:47 +0900

    logo 스타일 수정

--- components/Header.vue
+++ components/Header.vue
@@ -5,11 +5,17 @@
         <li class="logo"><button type="button" @click="gotoSection" data-section-color="light">Home</button>
           <svg
             id="Layer_1"
-            viewBox="0 0 95.9 95.9">
+            viewBox="0 0 95.9 95.9"
+            class="icon_logo"
+            >
             <path
               class="cls-1"
               d="m47.95,0C21.47,0,0,21.47,0,47.95s21.47,47.95,47.95,47.95,47.95-21.47,47.95-47.95S74.43,0,47.95,0Zm-22.81,59.14c0,1.36-1.11,2.46-2.49,2.46s-2.49-1.1-2.49-2.46v-21.67c0-1.36,1.11-2.46,2.49-2.46s2.49,1.1,2.49,2.46v21.67Zm17.95-6.89c.97.96.97,2.52,0,3.48-.97.96-2.55.96-3.52,0l-3.98-3.94-3.98,3.94c-.97.96-2.55.96-3.52,0-.97-.96-.97-2.52,0-3.48l3.98-3.94-4-3.96c-.97-.96-.97-2.52,0-3.48.97-.96,2.55-.96,3.52,0l4,3.96,4-3.96c.97-.96,2.55-.96,3.52,0,.97.96.97,2.52,0,3.48l-4,3.96,3.98,3.94Zm7.91,6.89c0,1.36-1.11,2.46-2.49,2.46s-2.49-1.1-2.49-2.46v-21.67c0-1.36,1.11-2.46,2.49-2.46s2.49,1.1,2.49,2.46v21.67Zm16.41-8.05h-11.94c-1.37,0-2.49-1.1-2.49-2.46s1.11-2.46,2.49-2.46h11.94c1.37,0,2.49,1.1,2.49,2.46s-1.11,2.46-2.49,2.46Zm9.45,8.05c0,1.36-1.11,2.46-2.49,2.46s-2.49-1.1-2.49-2.46v-21.67c0-1.36,1.11-2.46,2.49-2.46s2.49,1.1,2.49,2.46v21.67Z"/>
-          </svg></li>
+          </svg>
+          <svg viewBox="0 0 24 24" class="icon_up">
+            <path d="m6.293 13.293 1.414 1.414L12 10.414l4.293 4.293 1.414-1.414L12 7.586z"></path>
+          </svg>
+        </li>
         <li><button type="button" @click="gotoSection" data-section-color="light">profile</button></li>
         <li><button type="button" @click="gotoSection" data-section-color="dark">skills</button></li>
         <li><button type="button" @click="gotoSection" data-section-color="light">works</button></li>
@@ -47,6 +53,11 @@ onMounted(() => {
       entries.forEach(entry => {
         const sectionId = entry.target.id;
         if (entry.isIntersecting) {
+          if (sectionId !== 'intro') {
+            document.querySelector('nav ul li.logo').classList.add('toup');
+          } else {
+            document.querySelector('nav ul li.logo').classList.remove('toup');
+          }
           const navBtns = document.querySelectorAll('nav ul li button');
           document.querySelectorAll('nav ul li').forEach((li) => {
             li.classList.remove('on');
